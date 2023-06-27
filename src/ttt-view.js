5bac6a3bbedb18c66620de5aed3ffd719694afc1
ashley <ashleykimca@gmail.com>
2023-06-27 17:04:08 -0400

    almost done with ttt

--- src/ttt-view.js
+++ src/ttt-view.js
@@ -2,27 +2,33 @@ class View {
   constructor(game, el) {
     this.game = game;
     this.el = el; // .ttt figure is el
+    this.handleClick = this.handleClick.bind(this);
     this.setupBoard()
   }
   
   setupBoard() {
-
-
     let ul = document.createElement('ul')
     this.el.appendChild(ul)
-    
-  for (let i = 0; i < 9; i++) {
-    let pos = document.createElement('li')
-    ul.appendChild(pos)
+    ul.addEventListener("click", this.handleClick);
 
-    }
-
-
-
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          let pos = document.createElement('li')
+          ul.appendChild(pos)
+          pos.dataset.pos = `[${i}, ${j}]`
+        }
+      }       
   }
   
   handleClick(e) {
+    let position = e.target;
+    debugger
+    this.game.playMove(position.dataset.pos[0], position.dataset.pos[1]);
+
+
   }
+
+
 
   makeMove(square) {
   }
