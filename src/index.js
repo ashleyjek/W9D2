e145148853760085ba92d536568b5cc4532d3b08
ashley <ashleykimca@gmail.com>
2023-06-27 14:22:51 -0400

    created ul for ttt grid

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import View from "./ttt-view";
+const view = new View();
+
+import Game from "/ttt_node/game.js";
+const game = new Game();
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Your code here
+  // const canvas = document.getElementById("board-canvas");
+  // const ctx = canvas.getContext("2d");
+  // canvas.width = 1000;
+  // canvas.height = 1000;
+  // ctx.beginPath();
+  // ctx.rect(250, 10, 500, 500);
+  // ctx.stroke();
+  
+});
---
+++ src/ttt-view.js
@@ -0,0 +1,21 @@
+class View {
+  constructor(game, el) {
+    this.game = game;
+    this.setupBoard(el);
+  }
+  
+  setupBoard() {
+
+  }
+  
+  handleClick(e) {
+  }
+
+  makeMove(square) {
+  }
+  
+  handleGameOver() {
+  }
+}
+
+export default View;
